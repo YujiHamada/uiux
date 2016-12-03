f4a5cb0720b021ce1032e135d0d98b8cea125546
濱田裕史 <uziu2hahaha@gmail.com>
2016-12-03 14:44:53 +0900

    最初のコミット

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'ホーム')
+
+@section('sidebar')
+
+@endsection
+
+@section('content')
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <script type="text/javascript" src = "/js/app.js"></script>
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">yyUIUX</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <form class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class = "col-xs-2">
+<ul class="nav nav-pills nav-stacked">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul>
+</div>
+
+        @section('sidebar')
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
