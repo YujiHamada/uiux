5fa03675c80f9568dfb8774c89223952a67e158d
yutamaro0405 <yutamaro0405@gmail.com>
2017-01-16 19:16:53 +0900

    qq modify css

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,20 +3,20 @@
 @section('css')
   <style>
     img {
-        width: auto;
-        height: auto;
-        max-width: 120px;
-        max-height: 150px;
+      width: auto;
+      height: auto;
+      max-width: 150px;
+      max-height: 150px;
     }
   </style>
 @endsection
 
 @section('content')
-  <h4>ユーザネーム：{{ $user->name }}</h4>
+  <h4>タイムライン</h4>
   @forelse ($reviews as $review)
       <div class="media">
         <a class="media-left" href="{{ action('ReviewController@show', $review->id) }}">
-          <img class="media-object" src="{{Config::get('const.IMAGE_FILE_DIRECTORY')}}{{ $review->image_name }}" alt="がぞう">
+          <img class="media-object img-rounded" src="{{Config::get('const.IMAGE_FILE_DIRECTORY')}}{{ $review->image_name }}" alt="がぞう">
         </a>
         <div class="media-body">
           <h4 class="media-heading">{{ $review->title }}</h4>
