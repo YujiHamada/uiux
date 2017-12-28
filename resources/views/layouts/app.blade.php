c8445f17d353a6d5568f25cf6b90b380dabc0ec7
濱田裕史 <uziu2hahaha@gmail.com>
2017-12-28 21:17:28 +0900

    ベータ版なのでタイトルにベータの表記を追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
             <div class="container d-flex">
                 <a class="navbar-brand text-white mr-auto" href="/">
                     <img src="{{ asset('images/app_images/yyuxlogo_white.png') }}" style="height: 1.8rem;" class="mr-2" />
-                    yyUX
+                    yyUX（β版）
                 </a>
                 {{-- <div class="ml-auto"></div> --}}
 
