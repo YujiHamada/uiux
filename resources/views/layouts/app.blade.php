f524bfc56016fcb0d9c29bcfb5bfea87570ab4d1
濱田裕史 <uziu2hahaha@gmail.com>
2018-01-24 23:09:54 +0900

    モバイルサイトでレスポンシブデザインにならない不具合の解消

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     @endif
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
